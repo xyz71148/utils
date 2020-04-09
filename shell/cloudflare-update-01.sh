25f31efc5b0ad91e6ab5efd87a010dde6ed3145d
xyz71148 <59614093+xyz71148@users.noreply.github.com>
2020-04-10 07:34:45 +0800

    Create cloudflare-update-01.sh

---
+++ shell/cloudflare-update-01.sh
@@ -0,0 +1,46 @@
+# Cloudflare zone is the zone which holds the record
+zone=www.cc
+# dnsrecord is the A record which will be updated
+dnsrecord=www.www.cc
+
+## Cloudflare authentication details
+## keep these private
+cloudflare_auth_email=wwww@gmail.com
+cloudflare_auth_key=
+
+
+# Get the current external IP address
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
+
+echo "Current IP is $ip"
+
+if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+  echo "$dnsrecord is currently set to $ip; no changes needed"
+  exit
+fi
+
+# if here, the dns record needs updating
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
