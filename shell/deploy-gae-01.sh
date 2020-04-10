8636219744a09613b601c7c78a624dfcf075a5a6
xyz71148 <59614093+xyz71148@users.noreply.github.com>
2020-04-10 09:01:28 +0800

    Create deploy-gae-01.sh

---
+++ shell/deploy-gae-01.sh
@@ -0,0 +1,10 @@
+#!/bin/bsh
+
+USSAGE="curl https://raw.githubusercontent.com/xyz71148/utils/master/deploy-gae-01.sh | bash -s $PROJECT_ID"
+
+gcloud config set project $1 \
+  && rm -rf /tmp/vpn && git clone git@e.coding.net:sanfun/vpn.git /tmp/vpn \
+  && cd /tmp/vpn \
+  && gcloud app deploy -q \
+  && rm -rf /tmp/vpn \
+  && gcloud app logs tail -s default \
