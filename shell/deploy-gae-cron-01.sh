507887c9fae69b3ca4b8fe37834197367fd07f3c
xyz71148 <59614093+xyz71148@users.noreply.github.com>
2020-04-10 09:14:39 +0800

    Update deploy-gae-cron-01.sh

--- shell/deploy-gae-cron-01.sh
+++ shell/deploy-gae-cron-01.sh
@@ -1,6 +1,6 @@
 #!/bin/bsh
 
-USSAGE="curl https://raw.githubusercontent.com/xyz71148/utils/master/deploy-gae-cron-01.sh | bash -s $PROJECT_ID"
+USSAGE="curl https://raw.githubusercontent.com/xyz71148/utils/master/shell/deploy-gae-cron-01.sh | bash -s $PROJECT_ID"
 
 gcloud config set project $1 \
   && rm -rf /tmp/vpn && git clone git@e.coding.net:sanfun/vpn.git /tmp/vpn \
